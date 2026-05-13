cef5dfcbdc8bfbb393cee1991eb7ad2de6ed6e4c
Phillip Peng <ppeng08@gmail.com>
2026-05-13 00:16:59 -0400

    Update carrier count from seven to ten in footer disclaimer

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -12,7 +12,7 @@ export const site = {
 };
 
 export const medicareDisclaimer =
-  "We do not offer every plan available in your area. Currently we represent seven organizations which offer 19 products in your area. Please contact Medicare.gov, 1-800-MEDICARE, or your local State Health Insurance Program (SHIP) to get information on all of your options.";
+  "We do not offer every plan available in your area. Currently we represent ten organizations which offer 19 products in your area. Please contact Medicare.gov, 1-800-MEDICARE, or your local State Health Insurance Program (SHIP) to get information on all of your options.";
 
 export const medicareDisclaimerZh =
-  "我们并不提供您所在地区所有可用计划。目前我们代表七家机构，在您所在地区提供 19 项产品。请联系 Medicare.gov、1-800-MEDICARE，或您当地的州健康保险援助计划（SHIP），以了解所有可选方案。";
+  "我们并不提供您所在地区所有可用计划。目前我们代表十家机构，在您所在地区提供 19 项产品。请联系 Medicare.gov、1-800-MEDICARE，或您当地的州健康保险援助计划（SHIP），以了解所有可选方案。";
